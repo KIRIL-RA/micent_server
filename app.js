86e3c9b5ea0098d6908ee8a9bfb79a5ca3f7c446
KIRIL-RA <40370448+KIRIL-RA@users.noreply.github.com>
2021-11-27 12:58:14 +0300

    fixed a bug that prevented real-time control

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var getRouter = require('./routes/get')
 var testRouter = require('./routes/test/subscribe')
 
 app.use("/", express.static(__dirname + '/html/test'));
-//app.use("/get", getRouter);
+app.use("/get", getRouter);
 app.use("/test", testRouter);
 
 app.listen(80)
